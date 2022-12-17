6a4dfd677186355ad17755f87fda781ee0f22c4e
maxime <maxime@email.com>
2022-12-17 12:59:48 +0100

    git ignore update with /package-lock.json

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-const name = "helloWorld420"
+const name = "helloWorld_2022"
 function App() {
   return (
     <div className="App">
