f7709de7e187ab4564ad0931afb08b23f7f69f5d
maxime <maxime@email.com>
2022-12-18 16:19:29 +0100

    improve triangle collision and linear-function (case vertical line)

--- src/max/fourmiz/core/global_tools/analyse_and_trigo/LinearFunction.ts
+++ src/max/fourmiz/core/global_tools/analyse_and_trigo/LinearFunction.ts
@@ -2,13 +2,19 @@ import {Point} from "../Point";
 
 export class LinearFunction {
 
-    private static getDirectorCoefficientFrom2Points(left:Point, right:Point): number{
-        return ((right.y - left.y)/(right.x - left.x))
+    private static getDirectorCoefficientFrom2Points(left: Point, right: Point): number | undefined {
+        const deltaX = right.x - left.x
+        return (deltaX === 0) ? undefined : ((right.y - left.y) / (right.x - left.x))
     }
-    static getEquationFrom2Points(left:Point, right:Point): (x:number)=>number {
+
+    static getEquationFrom2Points(left: Point, right: Point): ((x: number) => number) | undefined {
         const a = this.getDirectorCoefficientFrom2Points(left, right)
-        const b = left.y - (a * left.x)
-        return (x:number)=> (a*x + b)
+        if (a === undefined) {
+            return undefined
+        } else {
+            const b = left.y - (a * left.x)
+            return (x: number) => (a * x + b)
+        }
     }
 
 }
